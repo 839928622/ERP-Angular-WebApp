91d8fc03d5620aec6a171d95375e20919aa0fd36
leocanbeverything <839928622@qq.com>
2020-06-04 09:14:54 +0800

    correct openNewTab

--- src/app/services/tabgroup.service.ts
+++ src/app/services/tabgroup.service.ts
@@ -10,14 +10,21 @@ export class TabGroupService {
   tabListObservable = this.tabList.asObservable(); // 供其他组件订阅
   constructor() { }
   openNewTab(tab: Tab) {
+     tab.active = true;
    // first： get priviously tabList/array
    this.tabListObservable.subscribe( lastTabList => {
- const lastActiveRouteInddex = lastTabList.findIndex(x => x.active === true);
- lastTabList[lastActiveRouteInddex].active = !lastTabList[lastActiveRouteInddex].active;
- tab.active = true;
- // second: push newly tab to array
- lastTabList.push(tab);
- this.tabList.next(lastTabList); // 广播
+ if (lastTabList.length > 0) {
+   const lastActiveRouteInddex = lastTabList.findIndex(x => x.active === true);
+   lastTabList[lastActiveRouteInddex].active = !lastTabList[lastActiveRouteInddex].active;
+  
+   // second: push newly tab to array
+   lastTabList.push(tab);
+   this.tabList.next(lastTabList); // 广播
+     }
+     lastTabList.push(tab);
+     const initialTabList:Tab[];
+     initialTabList.push(tab);
+     this.tabList.next(initialTabList); // 广播
   } );
 
   }
