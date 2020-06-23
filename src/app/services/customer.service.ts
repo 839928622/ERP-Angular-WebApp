827899da20b944cc9e83b942a051becf5f507099
Parker <jwk839928622@outlook.com>
2020-06-23 22:09:33 +0800

    setup customer sevice

---
+++ src/app/services/customer.service.ts
@@ -0,0 +1,19 @@
+import { environment } from './../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Customer } from '../models/customer';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  constructor(private httpClient: HttpClient,
+              ) { }
+
+  readCustomerByKeyword(keywork: string): Observable<Customer[]>
+  {
+    return this.httpClient.get<Customer[]>(environment.erpApiBase + '/customer/keyword=' + keywork);
+  }
+}
---
+++ src/app/services/orders.service.ts
@@ -0,0 +1,10 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrdersService {
+
+  constructor(private httpClient: HttpClient) { }
+}
