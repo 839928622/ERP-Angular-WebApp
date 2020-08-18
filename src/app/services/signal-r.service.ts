0316c53bf1d50c2fb22c1612ef786705b82b7cfa
Parker <jwk839928622@outlook.com>
2020-08-18 23:20:40 +0800

    explore signalr group

--- src/app/services/signal-r.service.ts
+++ src/app/services/signal-r.service.ts
@@ -7,6 +7,7 @@ import * as signalR from '@aspnet/signalr';
 })
 export class SignalRService {
 public data: BranchSettings;
+public connectionId: string;
 private hubConnection: signalR.HubConnection;
 public startConnection = () => {
   this.hubConnection = new signalR.HubConnectionBuilder()
@@ -17,7 +18,14 @@ public startConnection = () => {
 
   this.hubConnection
   .start()
-  .then(() => console.log('开启signal-r连接'))
+  .then(() => {
+    console.log('开启signal-r连接');
+    this.hubConnection.invoke('getConnectionId') // call AdvancedSettingHub's function "GetConnectionId",since we're in js/ts environment,
+                                                // fitst character need to be lowwer case
+    .then( (connectionId) => {
+     this.connectionId = connectionId; // assign connectionId(from api) to connectionId(local)
+    });
+  })
   .catch(error => console.log('signal-r连接错误！提示:' + error));
 }
 
